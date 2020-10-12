a1f62aa00a9f9e788abe1198d712025509511618
YanaSkyrda <yana.skirda101010@gmail.com>
2020-10-12 03:10:17 +0300

    Minor fixes.

--- Java_problems/Task4(class-info-loader)/src/test/java/ClassInfoPrinterTests.java
+++ Java_problems/Task4(class-info-loader)/src/test/java/ClassInfoPrinterTests.java
@@ -3,7 +3,6 @@ import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
-import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
