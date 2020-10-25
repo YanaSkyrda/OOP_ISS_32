33274b9dc8d398f1015abeffa21e20bdb7039f2f
YanaSkyrda <yana.skirda101010@gmail.com>
2020-10-25 21:02:02 +0200

    Custom cyclic barrier implemented

---
+++ Java_problems/Task7(custom-cyclic-barrier)/src/main/java/cyclicbarrier/CustomCyclicBarrier.java
@@ -0,0 +1,64 @@
+package cyclicbarrier;
+
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.TimeUnit;
+
+public class CustomCyclicBarrier {
+    private int threadsAmount;
+    private int threadsLeft;
+    private Runnable barrierEvent;
+    private boolean isBroken;
+
+    public CustomCyclicBarrier(int threadsAmount, Runnable barrierEvent) {
+        this.threadsAmount = threadsAmount;
+        this.threadsLeft = threadsAmount;
+        this.barrierEvent = barrierEvent;
+        this.isBroken = false;
+    }
+
+    public CustomCyclicBarrier(int threadsAmount) {
+        this.threadsAmount = threadsAmount;
+        this.threadsLeft = threadsAmount;
+        this.barrierEvent = null;
+    }
+
+    public synchronized void await()
+            throws InterruptedException, BrokenBarrierException {
+        if (this.isBroken) {
+            throw new BrokenBarrierException();
+        }
+
+        --this.threadsLeft;
+
+        if (this.threadsLeft > 0) {
+            try {
+                this.wait();
+            } catch (InterruptedException e) {
+                this.isBroken = true;
+                throw e;
+            }
+        } else {
+            this.threadsLeft = this.threadsAmount;
+            notifyAll();
+            if (this.barrierEvent != null) {
+                this.barrierEvent.run();
+            }
+        }
+    }
+
+    public void reset() {
+        this.threadsLeft = this.threadsAmount;
+    }
+
+    public boolean isBroken() {
+        return this.isBroken;
+    }
+
+    public int getThreadsAmount() {
+        return this.threadsAmount;
+    }
+
+    public int getNumberWaiting() {
+        return this.threadsLeft;
+    }
+}
