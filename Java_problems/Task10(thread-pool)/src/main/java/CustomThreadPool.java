9c4b7653f30988371bf248d44f175d84af07448e
YanaSkyrda <yana.skirda101010@gmail.com>
2020-11-25 19:24:25 +0200

    custom thread pool implemented

---
+++ Java_problems/Task10(thread-pool)/src/main/java/CustomThreadPool.java
@@ -0,0 +1,53 @@
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.Executor;
+
+public class CustomThreadPool implements Executor {
+    private final Queue<Runnable> tasksQueue = new ConcurrentLinkedQueue<>();
+    private int threadsAmount;
+    private Thread[] workerThreads;
+    private volatile boolean stillWorking = true;
+
+    public CustomThreadPool(int threadsAmount) {
+        this.threadsAmount = threadsAmount;
+        this.workerThreads = new Thread[threadsAmount];
+        for (int i = 0; i < threadsAmount; i++) {
+            this.workerThreads[i] = new Thread(new Worker());
+            this.workerThreads[i].start();
+        }
+    }
+
+    @Override
+    public void execute(Runnable command) {
+        if (stillWorking) {
+            tasksQueue.offer(command);
+        }
+    }
+
+    public void shutdown() {
+        stillWorking = false;
+        for (int i = 0; i < threadsAmount; i++) {
+            workerThreads[i].interrupt();
+        }
+    }
+
+    public boolean isStillWorking() {
+        return stillWorking;
+    }
+
+    public int getCurrentTasksAmount() {
+        return tasksQueue.size();
+    }
+
+    public final class Worker implements Runnable {
+        @Override
+        public void run() {
+            while (stillWorking) {
+                Runnable task = tasksQueue.poll();
+                if (task != null) {
+                    task.run();
+                }
+            }
+        }
+    }
+}
