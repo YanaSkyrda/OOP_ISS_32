89267d7e9b6debb5bd66ba0d9a90214a52ea00c0
DarynaBond <35077916+WAG13@users.noreply.github.com>
2020-11-27 20:15:26 +0200

    added tests

--- Labs/Lab3/app/src/test/java/com/oop/lab3/ExampleUnitTest.java
+++ Labs/Lab3/app/src/test/java/com/oop/lab3/ExampleUnitTest.java
@@ -1,17 +1,51 @@
 package com.oop.lab3;
 
+import org.junit.Assert;
 import org.junit.Test;
-
 import static org.junit.Assert.*;
 
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
- */
+import com.oop.lab3.dtw.DTW;
+
+import java.util.Arrays;
+import java.util.List;
+
 public class ExampleUnitTest {
+
     @Test
-    public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
+    public void testDTWEqualGraphs() {
+        float[] first = {1, 2, 3, 4, 3, 2, 1, 1, 1, 2};
+        float[] second = {1, 2, 3, 4, 3, 2, 1, 1, 1, 2};
+        DTW dtw =  new DTW();
+        double distance = dtw.compute(first, second).getDistance();
+        System.out.println(dtw.compute(first, second).getDistance());
+        Assert.assertEquals(0, distance, Math.pow(10,-10));
+    }
+
+    @Test
+    public void testDTWParalellGraphs() {
+        float[] first = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
+        float[] second = {10, 10, 10, 10, 10, 10, 10, 10, 10, 10};
+        DTW dtw =  new DTW();
+        double distance = dtw.compute(first, second).getDistance();
+        Assert.assertEquals(81, distance, Math.pow(10,-10));
+    }
+
+    @Test
+    public void testDTW() {
+        float[] first = {1, 2, 3, 4, 3, 2, 1, 1, 1, 2};
+        float[] second = {0, 1, 1, 2, 3, 4, 3, 2, 1, 1};
+        DTW dtw =  new DTW();
+        double distance = dtw.compute(first, second).getDistance();
+        System.out.println(dtw.compute(first, second).getDistance());
+        Assert.assertEquals(0.15384, distance, Math.pow(10,-5));
+    }
+    
+    @Test
+    public void testDTWDifferentLength() {
+        float[] first = {1, 2, 3, 4, 3, 2, 1};
+        float[] second = {1, 2, 3, 4, 3, 2, 1, 1, 1, 2};
+        DTW dtw =  new DTW();
+        double distance = dtw.compute(first, second).getDistance();
+        Assert.assertEquals(0.1, distance, Math.pow(10,-10));
     }
 }
