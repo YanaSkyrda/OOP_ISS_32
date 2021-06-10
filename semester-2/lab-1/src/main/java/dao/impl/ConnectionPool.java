531534b0cc40a8ec7efafa1faf5537b4e7d40194
YanaSkyrda <yana.skirda101010@gmail.com>
2021-06-11 01:23:44 +0300

    lab 1: fixed connection pool

--- semester-2/lab-1/src/main/java/dao/impl/ConnectionPool.java
+++ semester-2/lab-1/src/main/java/dao/impl/ConnectionPool.java
@@ -7,17 +7,16 @@ import java.lang.reflect.Proxy;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 public class ConnectionPool {
     private final Logger logger = LogManager.getLogger(ConnectionPool.class);
-    private final List<Connection> availableConnections = new ArrayList<>();
-    private final List<Connection> usedConnections = new ArrayList<>();
+    private final List<Connection> availableConnections = new CopyOnWriteArrayList<>();
     private final ResourceBundle bundle = ResourceBundle.getBundle("sql");
 
-    private final int MAX_SIZE = 5;
+    private final int MAX_SIZE = 15;
 
     public ConnectionPool() {
         for (int count = 0; count < MAX_SIZE; count++) {
@@ -26,45 +25,39 @@ public class ConnectionPool {
     }
 
     private Connection createConnection() {
-        Connection conn = null;
+        Connection connection = null;
         try {
             Class.forName(bundle.getString("database.driver"));
-            conn = DriverManager.getConnection(bundle.getString("database.url"),
+            connection = DriverManager.getConnection(bundle.getString("database.url"),
                     bundle.getString("database.user"), bundle.getString("database.pass"));
         } catch (SQLException | ClassNotFoundException e) {
             logger.info("Connection can`t be created");
         }
-        Connection finalConn = conn;
+        Connection finalConnection = connection;
         return (Connection) Proxy.newProxyInstance(Connection.class.getClassLoader(), new Class[]{Connection.class},
                 ((proxy, method, args) -> {
                     if (method.getName().equals("close")) {
-                        if (finalConn.isClosed()) {
-                            throw new IllegalArgumentException("Connection is already closed");
-                        }
-                        synchronized (this) {
-                            usedConnections.remove(proxy);
+                        if (finalConnection.isClosed()) {
+                            logger.warn("Connection is already closed");
+                        } else {
                             availableConnections.add((Connection) proxy);
                         }
+                        return null;
+                    } else {
+                        return method.invoke(finalConnection, args);
                     }
-                    return method.invoke(finalConn, args);
                 }));
     }
-
 
     public Connection getConnection() {
         if (availableConnections.size() == 0) {
             logger.info("All connections are used");
             return null;
         } else {
-            Connection con =
-                    availableConnections.remove(
-                            availableConnections.size() - 1);
-            usedConnections.add(con);
-            return con;
+            return availableConnections.remove(availableConnections.size() - 1);
         }
 
     }
-
 
     public int getFreeConnectionCount() {
         return availableConnections.size();
